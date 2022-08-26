5ca0c80d629045ead8b3a8834dd465108425cb9c
Tec <Tec@SKAB1-PC-01>
2022-08-26 08:26:51 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,75 @@
+﻿namespace OOP003
+{
+    internal class Person
+    {
+        //Private felter, der indeholder data/værdier
+
+        private string? name;
+        private DateTime? dob;
+        private string? email;
+        private string? password;
+
+        //Offentlige egenskaber, der henter (returfelt) og sætter (fra værdi) dataværdierne for vores private felter.
+        public string? Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == "")
+                {
+                    Console.WriteLine("Prøv igen...");
+                    name = null;
+                }
+                else name = value;
+            }
+        }
+        public DateTime? DoB
+        {
+            get { return dob; }
+            set
+            {
+                if (value < DateTime.Now) dob = value;
+                else
+                {
+                    Console.WriteLine("Fødselsdatoen er ikke godkendt, skal være før i dag.");
+                    dob = null;
+                }
+            }
+        }
+        public string? Email
+        {
+            get { return email; }
+            set
+            {
+                if (value != null && value.Contains("@") && value.Contains(".")) email = value;
+                else Console.WriteLine("Email skal indeholde @ og et .");
+            }
+        }
+        public string? Password
+        {
+            get { return password; }
+            set
+            {
+                if (value != null && value.Length > 6 && value.Any(char.IsUpper) && value.Any(char.IsLower) && value.Any(char.IsDigit) && !value.Contains(" ")) password = value;
+                else Console.WriteLine("Password skal være længere end 6 karakterer, indeholde både store og små bogstaver samt tal");
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                if (DoB != null)
+                {
+                    DateTime today = DateTime.Today;
+                    int age = today.Year - ((DateTime)DoB).Year;
+                    if (today < new DateTime(today.Year, ((DateTime)DoB).Month, ((DateTime)DoB).Day)) age--;
+                    return age;
+                }
+                else return 0;
+
+            }
+        }
+
+
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,76 @@
+﻿namespace OOP003
+{
+    internal class StartUp
+    {
+        public List<Person> People = new();
+        public StartUp()
+        {
+            while (true)
+            {
+                //CreatePerson-metoden opretter et nyt Person-objekt, fylder det med data og returnerer det til p
+                Person p = CreatePerson();
+                //p is added to our people list
+                People.Add(p);
+                //og vi viser navnet på alle personer på listen
+                foreach (Person person in People)
+                {
+                    Console.WriteLine($"Name: {person.Name}");
+                }
+            }
+        }
+
+        private Person CreatePerson()
+        {
+            //Opretter en ny instance af et Person-objekt.
+            Person person = new();
+            //Vi får input via vores metoder, der tilføjer værdier til vores Persons private felter, gennem Persons egenskaber.
+            GetName(person);
+            Console.WriteLine("Hi " + person.Name);
+            GetDoB(person);
+            Console.WriteLine($"Du er født: {person.DoB} og er {person.Age} år gammel.");
+            GetEmail(person);
+            Console.WriteLine("Din email er: " + person.Email);
+            GetPassword(person);
+            Console.WriteLine("Dit password er: " + person.Password);
+            return person;
+        }
+
+        private void GetName(Person person)
+        {
+            do
+            {
+                Console.Write("Indtast navn: ");
+                person.Name = Console.ReadLine();
+            }
+            while (person.Name == null);
+        }
+        private void GetDoB(Person person)
+        {
+            do
+            {
+                Console.Write("Indtast fødselsdato (dd/mm/åååå: ");
+                if (DateTime.TryParse(Console.ReadLine(), out DateTime dt)) person.DoB = dt;
+                else person.DoB = null;
+            }
+            while (person.DoB == null);
+        }
+        private void GetEmail(Person person)
+        {
+            do
+            {
+                Console.Write("Indtast email: ");
+                person.Email = Console.ReadLine();
+            }
+            while (person.Email == null);
+        }
+        private void GetPassword(Person person)
+        {
+            do
+            {
+                Console.Write("Indtast password: ");
+                person.Password = Console.ReadLine();
+            }
+            while (person.Password == null);
+        }
+    }
+}
